0b2daf56a11dfc7fee8e999d288251a5dde65a6e
Gui <guitimliu@gmail.com>
2023-06-20 00:33:50 +0800

    feat: 切版完成

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<script setup>
+import ListCard from './components/ListCard.vue'
+import Footer from './components/Footer.vue'
+import data from './assets/data.json'
+
+const informationList = [
+  {
+    content: data.information.list.address,
+  },
+  {
+    content: data.information.list.tel,
+  },
+  {
+    content: data.information.list.email,
+  },
+  {
+    content: data.information.list.website,
+  },
+];
+
+const awards = data.awards.list.map((item) => {
+  return {
+    content: item,
+  }
+});
+</script>
+
+<template>
+  <div class="home">
+    <h1 v-if="data.name || data.title" class="hidden">{{ data.name }} {{ data.title }}</h1>
+    <div class="container">
+      <p class="home__title" v-if="data.name" v-text="data.name" />
+      <!-- <img class="home__avatar" :src="data.avatar" :alt="data.name"> -->
+      <img class="home__avatar" src="./assets/avatar.png" :alt="data.name">
+      <p class="home__title" v-if="data.title" v-text="data.title" />
+    </div>
+    <div class="home__desc">
+      <div class="container">
+        <p v-if="data.desc" class="home__desc__text" v-text="data.desc"/>
+      </div>
+    </div>
+    <div class="container">
+      <div class="home__listCard">
+        <!-- Vito Evans -->
+        <ListCard :title="data.information.title" :list="informationList" :itemMarginBottom="2" />
+        <!-- Education -->
+        <ListCard :title="data.education.title" :list="data.education.list" :itemMarginBottom="4" />
+        <!-- Work -->
+        <ListCard :title="data.work.title" :list="data.work.list" :itemMarginBottom="4" />
+        <!-- Awards -->
+        <ListCard :title="data.awards.title" :list="awards" :itemMarginBottom="3" />
+      </div>
+    </div>
+    <Footer :socialLink="data.socialLink" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  &__title {
+    font-size: 192px;
+    line-height: 1.5;
+
+    &:nth-child(3) {
+      text-align: right;
+    }
+  }
+
+  &__avatar {
+    display: block;
+    width: 636px;
+    height: 848px;
+    margin: auto;
+  }
+
+  &__desc {
+    background-color: #EFEFEF;
+    padding: 48px 0;
+    margin-bottom: 80px;
+
+    &__text {
+      font-size: 32px;
+      line-height: 1.5;
+      width: 50%;
+      margin-left: auto;
+    }
+  }
+
+  &__listCard {
+    width: 50%;
+    margin-bottom: 160px;
+  }
+}
+</style>
---
+++ src/components/ListCard.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  list: {
+    type: Object,
+  },
+  itemMarginBottom: {
+    type: Number,
+  },
+});
+
+const spacing = ref(4);
+</script>
+
+<template>
+  <div class="listCard">
+    <h2 v-if="props.title" class="listCard__title" v-text="props.title" />
+    <ul v-if="list" class="listCard__list">
+      <li v-for="item in props.list" :key="item.content" class="listCard__item" :style="{marginBottom: `${props.itemMarginBottom * spacing}px`}">
+        <span v-if="item.date" class="listCard__item__small" v-text="item.date" />
+        <h3 v-if="item.content" class="listCard__item__content" v-text="item.content" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$spacing: 4px;
+.listCard {
+  margin-bottom: $spacing * 10;
+  line-height: 1.5;
+
+  &__title {
+    font-size: 40px;
+    color: #AE0000;
+    border-bottom: 2px solid #AE0000;
+    margin-bottom: $spacing * 4;
+  }
+
+  &__item {
+    &__small {
+      font-size: 20px;
+      color: #949494;
+    }
+
+    &__content {
+      font-size: 24px;
+    }
+  }
+}
+</style>
